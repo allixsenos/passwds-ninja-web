d96db82007a04b609e3704b1d513b65c53884d2c
Luka Kladaric <allixsenos@gmail.com>
2015-10-26 00:31:36 -0400

    extract script and css files, add "more" button

---
+++ script.js
@@ -0,0 +1,86 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-57622-11']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+})();
+
+var template = 'Cvccvc99';
+
+function generatePasswords(template, number) {
+    var chars = {}
+    chars['l'] = 'abcdefghijklmnoprstuvwxyz';
+    chars['U'] = chars['l'].toUpperCase();
+    chars['v'] = 'aeiouy';
+    chars['V'] = chars['v'].toUpperCase();
+    chars['c'] = 'bcdfghjklmnprstvwxz';
+    chars['C'] = chars['c'].toUpperCase();
+    chars['9'] = '0123456789';
+    chars['#'] = '!@#$%^&*_-+=()[]{}';
+    chars['a'] = chars['l'] + chars['9'];
+    chars['A'] = chars['a'].toUpperCase();
+
+    var i, c, possible;
+
+    var passwords = [];
+    for (i = 0; i < number; i++) {
+        password = '';
+        for (c = 0; c < template.length; c++) {
+            possible = chars[template.charAt(c)];
+            password += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        passwords.push(password);
+    }
+
+    return passwords;
+}
+
+function doPasswords() {
+  passwords = generatePasswords(template, 10);
+
+  var passwordlist = $("ul#passwords");
+  $.each(passwords, function(i, password) {
+      passwordlist.append($("<li>").text(password));
+  });
+}
+
+doPasswords();
+
+$('#passwords').on('click', 'li', function() {
+  var range = document.createRange();  
+  range.selectNode(this);  
+  window.getSelection().addRange(range);  
+    
+  var successful = false;
+  try {  
+    // Now that we've selected the anchor text, execute the copy command  
+    successful = document.execCommand('copy');  
+  } catch(err) {  
+    successful = false;
+    // whatever
+  }
+
+  if (!successful) {
+      prompt("Your browser does not support insta-copy. Sorry.", $(this).text());
+  }
+
+  // Remove the selections - NOTE: Should use
+  // removeRange(range) when it is supported  
+  window.getSelection().removeAllRanges();  
+});
+
+function disableSelect(e) { return false; }
+function reEnable() { return true; }
+document.onselectstart=disableSelect;
+if (window.sidebar){
+    document.onmousedown=disableSelect
+    document.onclick=reEnable
+}
+
+$('.more').on('click', function() {
+  $('#passwords li').remove();
+  doPasswords();
+});
